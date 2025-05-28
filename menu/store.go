3bb9ea3bc56f14d31749fc19c9903778ece2cd5a
pdridh <91915381+pdridh@users.noreply.github.com>
2025-05-28 19:27:57 +0545

    implement creation of items

---
+++ menu/store.go
@@ -0,0 +1,45 @@
+package menu
+
+import (
+	"context"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type Store interface {
+	CreateItem(ctx context.Context, name string, description string, price float64) (*MenuItem, error)
+}
+
+func New(db *sqlx.DB) *sqlxStore {
+	return &sqlxStore{
+		db: db,
+	}
+
+}
+
+type sqlxStore struct {
+	db *sqlx.DB
+}
+
+func (s *sqlxStore) CreateItem(ctx context.Context, name string, description string, price float64) (*MenuItem, error) {
+	const query = `INSERT INTO menu_items (name, description, price) VALUES(:name, :description, :price)	RETURNING *`
+
+	stmt, err := s.db.PrepareNamedContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	var i MenuItem
+
+	namedArgs := map[string]any{
+		"name":        name,
+		"description": description,
+		"price":       price,
+	}
+
+	if err := stmt.GetContext(ctx, &i, namedArgs); err != nil {
+		return nil, err
+	}
+
+	return &i, nil
+}
