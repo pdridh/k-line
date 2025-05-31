b30d3f28555d85b2a0e8cbf0f65e3e6e746479eb
pdridh <91915381+pdridh@users.noreply.github.com>
2025-05-31 12:54:11 +0545

    create util function to extract query params from request

--- api/requests.go
+++ api/requests.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"net/url"
+	"reflect"
+	"strconv"
 )
 
 // Read a request body and parse it.
@@ -21,4 +24,41 @@ func ParseJSON(r *http.Request, v any) error {
 	}
 
 	return nil
+}
+
+// Given the query params in the url and a struct that has strongly typed fields,
+// ParseQueryParams extracts the field from the query param if it exists and parses it into the type of the field it
+// using the json tag in the struct. Params should be a pointer to a struct to get loaded into.
+func ParseQueryParams(q url.Values, params any) {
+	v := reflect.ValueOf(params).Elem()
+
+	for i := range v.NumField() {
+		field := v.Field(i)
+		fieldType := v.Type().Field(i)
+		paramValue := q.Get(fieldType.Tag.Get("json")) // Get from query
+
+		if paramValue == "" {
+			continue
+		}
+
+		switch field.Kind() {
+		case reflect.Int64, reflect.Int32, reflect.Int:
+			val, err := strconv.Atoi(paramValue)
+			if err == nil {
+				field.SetInt(int64(val))
+			}
+		case reflect.Float64:
+			val, err := strconv.ParseFloat(paramValue, 64)
+			if err == nil {
+				field.SetFloat(val)
+			}
+		case reflect.Bool:
+			val, err := strconv.ParseBool(paramValue)
+			if err == nil {
+				field.SetBool(val)
+			}
+		default:
+			field.SetString(paramValue)
+		}
+	}
 }
